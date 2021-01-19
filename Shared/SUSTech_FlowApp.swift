138241a5878f45cf29576bde7be566d3ef683bcb
Alan Ai <aijunda29@gmail.com>
2021-01-19 15:49:17 +0800

    Initial Commit

---
+++ Shared/SUSTech_FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  SUSTech_FlowApp.swift
+//  Shared
+//
+//  Created by Alan on 2021/1/19.
+//
+
+import SwiftUI
+
+@main
+struct SUSTech_FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
